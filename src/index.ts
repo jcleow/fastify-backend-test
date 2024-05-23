61cc8f716a29e0e06e4751fee39f9e70499daa7c
Jit Corn Leow <jitcorn.leow@TMCC080L.local>
2024-05-23 10:37:21 +0800

    feat(add-swagger): add swagger

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,51 @@ import pino from 'pino'
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import loginRouter from './routes/login.routes'
 import {getUserById, getUser, createUser} from './routes/user.routes'
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const port = 8080
-
 const API_V1 = '/api/v1'
+
+// Set up swagger: https://stackoverflow.com/a/76170934
+const swaggerOptions = {
+  swagger: {
+      info: {
+          title: "My Title",
+          description: "My Description.",
+          version: "1.0.0",
+      },
+      host: "localhost",
+      schemes: ["http", "https"],
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      tags: [{ name: "Default", description: "Default" }],
+  },
+};
+
+const swaggerUiOptions = {
+  routePrefix: "/docs",
+  exposeRoute: true,
+};
 
 const startServer = async () => {
   const server = Fastify({
     logger: pino({level: 'info'})
   }).withTypeProvider<TypeBoxTypeProvider>()
   try{
+    // Register Swagger
+    await server.register(fastifySwagger, swaggerOptions);
+    await server.register(fastifySwaggerUi, swaggerUiOptions);
+
     // Register routes
     await server.register(loginRouter, {prefix: API_V1})
     await server.register(getUserById, {prefix: API_V1})
     await server.register(getUser, {prefix: API_V1})
     await server.register(createUser, {prefix: API_V1})
 
-
+    await server.ready()
     await server.listen({port: port})
+    server.swagger()
   } catch(e) {
     console.error(e)
   }
